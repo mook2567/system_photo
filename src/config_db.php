141329cad84bbecd29040e73e54fef770b2fb1d8
พีรดา แสนเสร็จ <peerada.sa@rmuti.ac.th>
2024-08-02 07:13:41 +0700

    aom

--- src/config_db.php
+++ src/config_db.php
@@ -1,7 +1,20 @@
 <?php
-$servername = "100.99.99.99:3309";
-$username = "rmuti";
-$password = "rmuti";
+// -----------------------  VPN  -----------------------------
+// $servername = "100.99.99.99:3309";
+// $username = "rmuti";
+// $password = "rmuti";
+// $dbname = "photo_match";
+
+// $conn = new mysqli($servername, $username, $password, $dbname);
+
+// // ตรวจสอบการเชื่อมต่อ
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+// ----------- localhost --------------------------
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
 $dbname = "photo_match";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
